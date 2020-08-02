bb94fe6029e8d5a5ab9fc23c4b06e90f6786ea20
康鑫 <k506855660@163.com>
2020-08-02 13:42:45 +0800

    启动时拉取代理

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -16,6 +16,7 @@ func main() {
 	gocron.Every(config.CheckProxy.CheckAllInterval).Seconds().Do(checker.CheckAll)
 	// 定时拉取
 	fetcher := fetch.NewFetcher(db, config, checker)
+	fetcher.FetchAllAndCheck()
 	gocron.Every(config.FetchProxy.FetchProxyInterval).Seconds().Do(fetcher.FetchAllAndCheck)
 
 	// pending
