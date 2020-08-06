12735e7125866b69dedc2858f5cf4dd093c960b8
康鑫 <k506855660@163.com>
2020-08-06 18:31:31 +0800

    定时拉取proxy，改为生成者消费者模式

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -5,20 +5,24 @@ import (
 	"proxy-pool/config"
 	"proxy-pool/databases"
 	"proxy-pool/fetch"
+	"proxy-pool/model"
 
 	"github.com/jasonlvhit/gocron"
 )
 
 func main() {
+	var ch = make(chan *model.Proxy)
 	config := config.New()
 	db := databases.New(config.Mysql)
 	checker := check.NewChecker(db, config)
 	gocron.Every(config.CheckProxy.CheckAllInterval).Seconds().Do(checker.CheckAll)
 	// 定时拉取
-	fetcher := fetch.NewFetcher(db, config, checker)
-	fetcher.FetchAllAndCheck()
-	gocron.Every(config.FetchProxy.FetchProxyInterval).Seconds().Do(fetcher.FetchAllAndCheck)
+	fetcher := fetch.NewFetcher(db, config, checker, ch)
 
-	// pending
-	<-gocron.Start()
+	gocron.Every(config.FetchProxy.FetchProxyInterval).Seconds().Do(fetcher.FetchAll)
+
+	// start
+	gocron.Start()
+	// 插入
+	fetcher.CheckAndInsert()
 }
